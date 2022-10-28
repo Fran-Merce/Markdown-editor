284dae50d54c795c1c39627950b3971ce85473c4
fran-merce <franmerce1@gmail.com>
2022-10-28 17:50:28 -0300

    place holder /n fixed

--- src/constanst/placeHolder.ts
+++ src/constanst/placeHolder.ts
@@ -1,3 +1,3 @@
-export const placeHolder = `# Welcome To my Markdown Editor \n### Hi there 👋 \n* My name is Lionel Messi\nI’m currently working on ...\n* 🌱 I’m currently learning ...\n* 🤝 I’m looking to collaborate on ...\n* 🤔 I’m looking for help with ...\n* 💬 Ask me about ...\n* 📫 How to reach me: ...\n* ❤️ I use... \n 
+export const placeHolder = `# Welcome To my Markdown Editor \n### Hi there 👋 \n* My name is Lionel Messi\n * I’m currently working on ...\n* 🌱 I’m currently learning ...\n* 🤝 I’m looking to collaborate on ...\n* 🤔 I’m looking for help with ...\n* 💬 Ask me about ...\n* 📫 How to reach me: ...\n* ❤️ I use... \n 
 ![](https://github-readme-stats.vercel.app/api/top-langs/?username=Fran-merce&theme=radical&hide_border=true&include_all_commits=false&count_private=false&layout=compact)
 `;
